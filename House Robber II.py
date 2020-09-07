0bb34fff973dd341645606da3bbf0f80507ead10
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2020-09-07 15:39:39 -0400

    Create House Robber II.py

---
+++ House Robber II.py
@@ -0,0 +1,40 @@
+"""
+House Robber II
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed. All houses at this place are arranged in a circle. That means the first house is the neighbor of the last one. Meanwhile, adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+
+Example 1:
+
+Input: [2,3,2]
+Output: 3
+Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 2),
+             because they are adjacent houses.
+Example 2:
+
+Input: [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+             Total amount you can rob = 1 + 3 = 4.
+"""
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if nums is None or len(nums) == 0:
+            return 0
+        if len(nums) == 1: return nums[0]
+
+        nums1 = nums[0:len(nums)-1]
+        nums2 = nums[1:len(nums)]
+        return max(self.helper(nums1), self.helper(nums2))
+
+    def helper(self, nums) -> int:
+        if nums is None or len(nums) == 0:
+            return 0
+        if len(nums) == 1: return nums[0]
+
+        dp = [nums[0], max(nums[0], nums[1])]
+
+        for i in range(2, len(nums)):
+            dp.append(max(dp[i - 1], dp[i - 2] + nums[i]))
+
+        return dp[len(nums)-1]
