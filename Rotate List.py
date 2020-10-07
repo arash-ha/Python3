83be86dd6f74c061ae61dd6abaa7708e3aef26a7
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2020-10-07 09:20:50 -0400

    Create Rotate List.py

---
+++ Rotate List.py
@@ -0,0 +1,47 @@
+"""
+Rotate List
+
+Given a linked list, rotate the list to the right by k places, where k is non-negative.
+
+Example 1:
+
+Input: 1->2->3->4->5->NULL, k = 2
+Output: 4->5->1->2->3->NULL
+Explanation:
+rotate 1 steps to the right: 5->1->2->3->4->NULL
+rotate 2 steps to the right: 4->5->1->2->3->NULL
+Example 2:
+
+Input: 0->1->2->NULL, k = 4
+Output: 2->0->1->NULL
+Explanation:
+rotate 1 steps to the right: 2->0->1->NULL
+rotate 2 steps to the right: 1->2->0->NULL
+rotate 3 steps to the right: 0->1->2->NULL
+rotate 4 steps to the right: 2->0->1->NULL
+
+"""
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def rotateRight(self, head: ListNode, k: int) -> ListNode:
+        if not head or not head.next or k == 0: return head
+        p: ListNode = head
+        c = 1
+        while p.next:
+            p = p.next
+            c += 1
+
+        p.next = head
+        k = c - (k % c)
+        while k:
+            p = p.next
+            k -= 1
+        head = p.next
+        p.next = None
+        return head
