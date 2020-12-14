5a61ef65bd1454303719c14e0cb0d4fd2a48eb5d
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2020-12-13 21:52:02 -0500

    Create Burst Balloons.py

---
+++ Burst Balloons.py
@@ -0,0 +1,32 @@
+"""
+Burst Balloons
+
+
+Given n balloons, indexed from 0 to n-1. Each balloon is painted with a number on it represented by array nums. You are asked to burst all the balloons. If the you burst balloon i you will get nums[left] * nums[i] * nums[right] coins. Here left and right are adjacent indices of i. After the burst, the left and right then becomes adjacent.
+
+Find the maximum coins you can collect by bursting the balloons wisely.
+
+Note:
+
+You may imagine nums[-1] = nums[n] = 1. They are not real therefore you can not burst them.
+0 <= n <= 500, 0 <= nums[i] <= 100
+Example:
+
+Input: [3,1,5,8]
+Output: 167 
+Explanation: nums = [3,1,5,8] --> [3,5,8] -->   [3,8]   -->  [8]  --> []
+             coins =  3*1*5      +  3*5*8    +  1*3*8      + 1*8*1   = 167
+"""
+
+class Solution:
+    def maxCoins(self, nums: List[int]) -> int:
+        nums1 = [1] + [i for i in nums if i > 0] + [1]
+        n = len(nums1)
+        dp = [[0]*n for _ in range(n)]
+
+        for k in range(2, n):
+            for l in range(0, n - k):
+                r = l + k
+                for i in range(l + 1, r):
+                    dp[l][r] = max(dp[l][r], nums1[l] * nums1[i] * nums1[r] + dp[l][i] + dp[i][r])
+        return dp[0][n - 1]
