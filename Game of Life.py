32350cc1a2ff2586dbcf57f5e418b31e62f7cc5f
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2020-12-30 12:40:10 -0500

    Create Game of Life.py

---
+++ Game of Life.py
@@ -0,0 +1,56 @@
+"""
+Game of Life
+
+According to Wikipedia's article: "The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970."
+
+The board is made up of an m x n grid of cells, where each cell has an initial state: live (represented by a 1) or dead (represented by a 0). Each cell interacts with its eight neighbors (horizontal, vertical, diagonal) using the following four rules (taken from the above Wikipedia article):
+
+Any live cell with fewer than two live neighbors dies as if caused by under-population.
+Any live cell with two or three live neighbors lives on to the next generation.
+Any live cell with more than three live neighbors dies, as if by over-population.
+Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+The next state is created by applying the above rules simultaneously to every cell in the current state, where births and deaths occur simultaneously. Given the current state of the m x n grid board, return the next state.
+
+Example 1:
+
+Input: board = [[0,1,0],[0,0,1],[1,1,1],[0,0,0]]
+Output: [[0,0,0],[1,0,1],[0,1,1],[0,1,0]]
+Example 2:
+
+
+Input: board = [[1,1],[1,0]]
+Output: [[1,1],[1,1]]
+ 
+
+Constraints:
+
+m == board.length
+n == board[i].length
+1 <= m, n <= 25
+board[i][j] is 0 or 1.
+ 
+
+Follow up:
+
+Could you solve it in-place? Remember that the board needs to be updated simultaneously: You cannot update some cells first and then use their updated values to update other cells.
+In this question, we represent the board using a 2D array. In principle, the board is infinite, which would cause problems when the active area encroaches upon the border of the array (i.e., live cells reach the border). How would you address these problems?
+"""
+
+class Solution:
+    def gameOfLife(self, board: List[List[int]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        m, n = len(board), len(board[0])
+        for i in range(m):
+            for j in range(n):
+                count = 0
+                for I in range(max(0, i - 1), min(i + 2, m)):
+                    for J in range(max(0, j - 1), min(j + 2, n)):
+                        count += board[I][J] - 2 if board[I][J] > 1 else board[I][J]
+                if count == 3 or count - board[i][j] == 3:
+                    board[i][j] += 2
+                    
+        for i in range(m):
+            for j in range(n):
+                board[i][j] = 1 if board[i][j] > 1 else 0
