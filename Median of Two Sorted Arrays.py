ffcb543a86be3510bac62cfa93e770c9a7a55e50
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2021-01-10 16:37:53 -0500

    Create Median of Two Sorted Arrays.py

---
+++ Median of Two Sorted Arrays.py
@@ -0,0 +1,68 @@
+"""
+Median of Two Sorted Arrays
+
+Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+Follow up: The overall run time complexity should be O(log (m+n)).
+
+Example 1:
+
+Input: nums1 = [1,3], nums2 = [2]
+Output: 2.00000
+Explanation: merged array = [1,2,3] and median is 2.
+
+Example 2:
+
+Input: nums1 = [1,2], nums2 = [3,4]
+Output: 2.50000
+Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+
+Example 3:
+
+Input: nums1 = [0,0], nums2 = [0,0]
+Output: 0.00000
+
+Example 4:
+
+Input: nums1 = [], nums2 = [1]
+Output: 1.00000
+
+Example 5:
+
+Input: nums1 = [2], nums2 = []
+Output: 2.00000
+ 
+
+Constraints:
+
+nums1.length == m
+nums2.length == n
+0 <= m <= 1000
+0 <= n <= 1000
+1 <= m + n <= 2000
+-106 <= nums1[i], nums2[i] <= 106
+"""
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        size = len(nums1) + len(nums2)
+        if size % 2 == 0:
+            left = self.helper(nums1, 0, nums2, 0, size // 2)
+            right = self.helper(nums1, 0, nums2, 0, size // 2 + 1)
+            return (left + right) / 2.0
+
+        else:
+            return self.helper(nums1, 0, nums2, 0, size // 2 + 1)
+
+    def helper(self, nums1, i, nums2, j, k) -> int:
+        if len(nums1) - i > len(nums2) - j:
+            return self.helper(nums2, j ,nums1, i, k)
+        if len(nums1) == i:
+            return nums2[j + k - 1]
+        if k == 1:
+            return min(nums1[i], nums2[j])
+        ii, jj = min(i + k // 2, int(len(nums1))), j + k // 2
+        if nums1[ii - 1] > nums2[jj - 1]:
+            return self.helper(nums1, i, nums2, j + k // 2, k - k // 2)
+        else:
+            return self.helper(nums1, ii, nums2, j, k - (ii - i))
