ec69878ffe6cb45e2913dfedf81bd9cc981da08a
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2021-01-14 11:10:19 -0500

    Create Minimum Operations to Reduce X to Zero.py

---
+++ Minimum Operations to Reduce X to Zero.py
@@ -0,0 +1,47 @@
+"""
+Minimum Operations to Reduce X to Zero
+
+
+You are given an integer array nums and an integer x. In one operation, you can either remove the leftmost or the rightmost element from the array nums and subtract its value from x. Note that this modifies the array for future operations.
+
+Return the minimum number of operations to reduce x to exactly 0 if it's possible, otherwise, return -1. 
+
+Example 1:
+
+Input: nums = [1,1,4,2,3], x = 5
+Output: 2
+Explanation: The optimal solution is to remove the last two elements to reduce x to zero.
+
+Example 2:
+
+Input: nums = [5,6,7,8,9], x = 4
+Output: -1
+
+Example 3:
+
+Input: nums = [3,2,20,1,1,3], x = 10
+
+Output: 5
+Explanation: The optimal solution is to remove the last three elements and the first two elements (5 operations in total) to reduce x to zero.
+ 
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 104
+1 <= x <= 109
+"""
+
+class Solution:
+    def minOperations(self, nums: List[int], x: int) -> int:
+        n, l = len(nums), 0
+        res = n + 1
+        sm = sum(nums) - x
+        for r in range(n):
+            sm -= nums[r]
+            while l <= r and sm < 0:
+                sm += nums[l]
+                l += 1
+            if sm == 0:
+                res = min(res, n - r + l - 1)
+
+        return (-1 if res == n + 1 else res)
