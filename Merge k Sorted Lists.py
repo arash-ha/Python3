2498759b73eddeba87031f2755fddb9bfc1b9feb
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2021-01-24 18:35:31 -0500

    Create Merge k Sorted Lists.py

---
+++ Merge k Sorted Lists.py
@@ -0,0 +1,72 @@
+"""
+Merge k Sorted Lists
+
+You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+Merge all the linked-lists into one sorted linked-list and return it.
+
+ 
+
+Example 1:
+
+Input: lists = [[1,4,5],[1,3,4],[2,6]]
+Output: [1,1,2,3,4,4,5,6]
+Explanation: The linked-lists are:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+merging them into one sorted list:
+1->1->2->3->4->4->5->6
+Example 2:
+
+Input: lists = []
+Output: []
+Example 3:
+
+Input: lists = [[]]
+Output: []
+ 
+
+Constraints:
+
+k == lists.length
+0 <= k <= 10^4
+0 <= lists[i].length <= 500
+-10^4 <= lists[i][j] <= 10^4
+lists[i] is sorted in ascending order.
+The sum of lists[i].length won't exceed 10^4.
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        if len(lists) == 0:
+            return None
+        resHead = lists[0]
+        for i in range(1, len(lists)):
+            resHead = self.mergeTwoLists(resHead, lists[i])
+        return resHead
+
+    def mergeTwoLists(self, l1, l2) -> ListNode:
+        dummy = cur = ListNode(-1)
+        while l1 and l2:
+            if l1.val <= l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+
+            cur = cur.next
+
+        if l1:
+            cur.next = l1
+        if l2:
+            cur.next = l2
+        return dummy.next
