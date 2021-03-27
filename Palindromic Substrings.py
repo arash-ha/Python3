b69b35dcb2ff7f5fc6b6913b855fe68beb05dff3
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2021-03-27 16:04:06 -0400

    Create Palindromic Substrings.py

---
+++ Palindromic Substrings.py
@@ -0,0 +1,35 @@
+"""
+Palindromic Substrings
+
+
+Given a string, your task is to count how many palindromic substrings in this string.
+The substrings with different start indexes or end indexes are counted as different substrings even they consist of same characters.
+
+Example 1:
+
+Input: "abc"
+Output: 3
+Explanation: Three palindromic strings: "a", "b", "c".
+ 
+Example 2:
+
+Input: "aaa"
+Output: 6
+Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+ 
+Note:
+
+The input string length won't exceed 1000.
+"""
+
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        c = 0
+        for i in range(2 * len(s) - 1):
+            left, right = i // 2, (i + 1) // 2
+            while(left >= 0 and right < len(s) and s[left] == s[right]):
+                c += 1
+                left -= 1
+                right += 1
+        return c
+    
