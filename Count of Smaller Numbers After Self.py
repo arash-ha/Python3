b08dd82e641e8320c1984aa31b716e1d083d480c
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2021-07-06 22:03:52 -0400

    Create Count of Smaller Numbers After Self.py

---
+++ Count of Smaller Numbers After Self.py
@@ -0,0 +1,47 @@
+"""
+Count of Smaller Numbers After Self
+
+You are given an integer array nums and you have to return a new counts array. The counts array has the property where counts[i] is the number of smaller elements to the right of nums[i].
+
+Example 1:
+
+Input: nums = [5,2,6,1]
+Output: [2,1,1,0]
+Explanation:
+To the right of 5 there are 2 smaller elements (2 and 1).
+To the right of 2 there is only 1 smaller element (1).
+To the right of 6 there is 1 smaller element (1).
+To the right of 1 there is 0 smaller element.
+
+Example 2:
+
+Input: nums = [-1]
+Output: [0]
+
+Example 3:
+
+Input: nums = [-1,-1]
+Output: [0,0]
+ 
+Constraints:
+
+1 <= nums.length <= 105
+-10^4 <= nums[i] <= 10^4
+"""
+
+class Solution:
+    def countSmaller(self, nums: List[int]) -> List[int]:
+        def sort(enum):
+            half = len(enum) // 2
+            if half:
+                left, right = sort(enum[:half]), sort(enum[half:])
+                for i in range(len(enum))[::-1]:
+                    if not right or left and left[-1][1] > right[-1][1]:
+                        smaller[left[-1][0]] += len(right)
+                        enum[i] = left.pop()
+                    else:
+                        enum[i] = right.pop()
+            return enum
+        smaller = [0] * len(nums)
+        sort(list(enumerate(nums)))
+        return smaller
