efc9f54efdc8e904cedd1c8664d94e42dca65f4b
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2021-08-27 16:16:36 -0400

    Create Valid Sudoku.py

---
+++ Valid Sudoku.py
@@ -0,0 +1,66 @@
+"""
+Valid Sudoku
+
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+Note:
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+
+Example 1:
+
+Input: board = 
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: true
+
+Example 2:
+
+Input: board = 
+[["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: false
+Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+ 
+Constraints:
+
+board.length == 9
+board[i].length == 9
+board[i][j] is a digit or '.'.
+"""
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        N = 9 
+        rowSet = [set() for _ in range(N)]
+        colSet = [set() for _ in range(N)]
+        squareSet = [set() for _ in range(N)]
+        for r in range(N):
+            for c in range(N):
+                val = board[r][c]
+                if val == '.':
+                    continue
+                sPos = (r // 3) * 3 + c // 3
+                if val in rowSet[r] or val in colSet[c] or val in squareSet[sPos]:
+                    return False
+                rowSet[r].add(val)
+                colSet[c].add(val)
+                squareSet[sPos].add(val)
+
+        return True
