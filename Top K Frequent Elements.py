49dfc12905c2269781fb4930d25eda99c17c2c57
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2021-11-04 16:18:03 -0400

    Create Top K Frequent Elements.py

---
+++ Top K Frequent Elements.py
@@ -0,0 +1,35 @@
+"""
+Top K Frequent Elements
+
+Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+Example 1:
+
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+
+Example 2:
+
+Input: nums = [1], k = 1
+Output: [1]
+
+Constraints:
+
+1 <= nums.length <= 10^5
+k is in the range [1, the number of unique elements in the array].
+It is guaranteed that the answer is unique.
+ 
+Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+
+"""
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        dic = {}
+        for n in nums:
+            if n in dic:
+                dic[n] = dic[n] + 1
+            else:
+                dic[n] = 1
+        res = sorted(dic, key = dic.get, reverse = True)
+        return res[:k]
