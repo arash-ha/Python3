12d03488f8f38dc2024c87e9fc3f58e4509ef62b
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2022-08-28 10:28:02 -0400

    Create Projection Area of 3D Shapes.py

---
+++ Projection Area of 3D Shapes.py
@@ -0,0 +1,42 @@
+"""
+Projection Area of 3D Shapes
+
+You are given an n x n grid where we place some 1 x 1 x 1 cubes that are axis-aligned with the x, y, and z axes.
+Each value v = grid[i][j] represents a tower of v cubes placed on top of the cell (i, j).
+We view the projection of these cubes onto the xy, yz, and zx planes.
+A projection is like a shadow, that maps our 3-dimensional figure to a 2-dimensional plane. We are viewing the "shadow" when looking at the cubes from the top, the front, and the side.
+Return the total area of all three projections.
+
+Example 1:
+
+Input: grid = [[1,2],[3,4]]
+Output: 17
+Explanation: Here are the three projections ("shadows") of the shape made with each axis-aligned plane.
+
+Example 2:
+
+Input: grid = [[2]]
+Output: 5
+
+Example 3:
+
+Input: grid = [[1,0],[0,2]]
+Output: 8
+ 
+Constraints:
+
+n == grid.length == grid[i].length
+1 <= n <= 50
+0 <= grid[i][j] <= 50
+"""
+class Solution:
+    def projectionArea(self, grid: List[List[int]]) -> int:
+        res = 0
+        for i in range(len(grid)):
+            m, n = 0, 0
+            for j in range(len(grid)):
+                if grid[i][j] > m: m = grid[i][j]
+                if grid[j][i] > n: n = grid[j][i]
+                if grid[i][j]: res += 1
+            res += m + n
+        return res
