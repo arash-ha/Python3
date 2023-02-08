3a2e5c9bc66c2af5489885b5d96f6b8ab649f592
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2023-02-08 08:48:59 -0500

    Create Concatenated Words.py

---
+++ Concatenated Words.py
@@ -0,0 +1,52 @@
+"""
+Concatenated Words
+
+Given an array of strings words (without duplicates), return all the concatenated words in the given list of words.
+A concatenated word is defined as a string that is comprised entirely of at least two shorter words in the given array.
+
+Example 1:
+
+Input: words = ["cat","cats","catsdogcats","dog","dogcatsdog","hippopotamuses","rat","ratcatdogcat"]
+Output: ["catsdogcats","dogcatsdog","ratcatdogcat"]
+Explanation: "catsdogcats" can be concatenated by "cats", "dog" and "cats"; 
+"dogcatsdog" can be concatenated by "dog", "cats" and "dog"; 
+"ratcatdogcat" can be concatenated by "rat", "cat", "dog" and "cat".
+
+Example 2:
+
+Input: words = ["cat","dog","catdog"]
+Output: ["catdog"]
+ 
+Constraints:
+
+1 <= words.length <= 10^4
+1 <= words[i].length <= 30
+words[i] consists of only lowercase English letters.
+All the strings of words are unique.
+1 <= sum(words[i].length) <= 10^5
+"""
+
+class Solution:
+    def findAllConcatenatedWordsInADict(self, words: List[str]) -> List[str]:
+        d = set(words)
+
+        def dfs(word):
+            for i in range(1, len(word)):
+                prefix = word[:i]
+                suffix = word[i:]
+
+                if prefix in d and suffix in d:
+                    return True
+                if prefix in d and dfs(suffix):
+                    return True
+                if suffix in d and dfs(prefix):
+                    return True
+
+            return False
+
+        res = []
+        for word in words:
+            if dfs(word):
+                res.append(word)
+
+        return res
