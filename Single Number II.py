b6f2b666b6e801e3f40037a7a7a1548e5e0a82a3
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2023-09-14 07:32:58 -0700

    Create Single Number II.py

---
+++ Single Number II.py
@@ -0,0 +1,31 @@
+"""
+Single Number II
+
+Given an integer array nums where every element appears three times except for one, which appears exactly once. Find the single element and return it.
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+Example 1:
+
+Input: nums = [2,2,3,2]
+Output: 3
+
+Example 2:
+
+Input: nums = [0,1,0,1,0,1,99]
+Output: 99
+ 
+Constraints:
+
+1 <= nums.length <= 3 * ^104
+-2^31 <= nums[i] <= 2^31 - 1
+Each element in nums appears exactly three times except for one element which appears once.
+"""
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        count = defaultdict(int)
+        for x in nums:
+            count[x] += 1
+        for x, freq in count.items():
+            if freq == 1:
+                return x
+        return -1
